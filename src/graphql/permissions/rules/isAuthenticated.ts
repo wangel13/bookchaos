82ad286d1a23e756ec01276ba333d010e3bdcbcc
Taras <tapacpro@gmail.com>
2020-07-17 18:01:34 +0300

    Fix: path for lib/auth

--- src/graphql/permissions/rules/isAuthenticated.ts
+++ src/graphql/permissions/rules/isAuthenticated.ts
@@ -1,4 +1,4 @@
-import { getLoginSession } from 'lib/auth'
+import { getLoginSession } from '../../../lib/auth'
 import { rule } from 'graphql-shield'
 
 export const isAuthenticated = rule({ cache: 'contextual' })(async (parent, args, ctx, info) => {
